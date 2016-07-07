8df1a22ac4470a7cbbc83ca73184a9bca24196dd
Gem Lorenz <gemlorenz10@gmail.com>
2016-07-07 18:26:27 +0800

    Cleaning up the mess

--- Bash/BackupDB.sh
+++ Bash/BackupDB.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 #filename BackupDB.sh
-#filename sample for backup _JAN_10_Wed_1630DBbackup.tar.gz
+#filename sample for backup Prefix_JAN_10_Wed_1630DBbackup.tar.gz
 #get time and date
         min=$(date +%M)
         hour=$(date +%H)
@@ -9,22 +9,20 @@
         mnum=$(date +%m)
         dow=$(date +%a)
         year=$(date +%Y)
-
-
-        #Put Parameters here
 #---------------------
 #Finding Prefix/labeling i.e Last_Wedof$backup_name
             week=$(date +%a)
             last_7day=$(date -d "`date +%Y%m01` +1 month -7day" +%d)
             today=$(date +%d)
-            #if today > last_7day
+            #if today > last 7 days of the Month
                 if [ $today -ge $last_7day ] && [ "$week" = "Wed" ];
                 then
                 #true
-                prefix_name="Last_Wedof"
+                prefix_name="Last_Wedof_"
                 else
                 prefix_name=""
                 fi
+#Put Parameters here
 #Database Parameters
         userdb=root
         password=root
@@ -32,17 +30,15 @@
         backup_name=$prefix_name$mname"_"$dom"_"$dow"_"$hour"."$min$database"."sql
         backup_dir=backupdb/
 #----------------------
-#Compressing Variables
+#Compress/Zip Variables
 #Name of backup when zipped
         zip_name=$prefix_name$mname"_"$dom"_"$dow"_"$hour"."$min$database"."tar.gz
 #---------------------
-#Transferring Variables
+#SCP Variables
         user=root
         server=192.168.0.251
         remote_dir=/home/gem/
-
 #Backing up rocess start
-
     #go to backup directory
     cd $backup_dir
     #create a database dump file
