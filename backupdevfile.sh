e5f9b55e3b265a422d0f6e98a8e8f22b84cbd313
Gem Lorenz <gemlorenz10@gmail.com>
2016-07-13 22:25:02 +0800

    Update backupdevfile.sh

--- backupdevfile.sh
+++ backupdevfile.sh
@@ -4,28 +4,28 @@
 
 #$raw the raw file or directory you want to backup
 #$remotedir is the directory in your remote backup server where you want your backup files to be saved
-raw=/www/file-upload
-backupname=file-upload
+	raw=/www/file-upload
+	backupname=file-upload
 #remote/backupdatabase
-remotedir=/backup/
-user=backup
-server=218.50.181.107
+	remotedir=/backup/
+	user=backup
+	server=218.50.181.107
 #--Dynamic variables
 #$date will be used as prefix name for backup files
-date=$(date +%a_%b_%d_)
+	date=$(date +%a_%b_%d_)
 #$backupname wil be the name of the archived and zipped raw file
-backupname=$date$backupname.tar.gz
+	backupname=$date$backupname.tar.gz
 #compressed zip the raw file or directory
-tar -czpf $backupname $raw 
-if [ "$?" -eq 0 ];then
+	tar -czpf $backupname $raw 
+	if [ "$?" -eq 0 ];then
 	#Send it to backup server
 	scp -q $backupneame $user@$server:$remotedir
 		if [ "$?" -eq 0 ];then
-			#remove backup zipped on local machine 
-			rm $backupname
+		#remove backup zipped on local machine 
+		rm $backupname
 		else
-			exit
+		exit
 		fi
 else
-	exit	
+exit	
 fi
