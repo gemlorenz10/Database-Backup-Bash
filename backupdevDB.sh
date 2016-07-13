ead5d83b1180df17f0c475dd34f1c6d14cbc0dca
Gem Lorenz <gemlorenz10@gmail.com>
2016-07-13 21:36:17 +0800

    Update backupdevDB.sh

--- backupdevDB.sh
+++ backupdevDB.sh
@@ -1,41 +1,38 @@
 #!bin/bash
-#run this script on ~ directory of the user
+#run this script on ~ directory of the current user
 #filename BackupDB.sh
 #filename sample for backup Prefix_JAN_10_Wed_1630DBbackup.tar.gz
+#-------------Static Variables
+#Database Variables
+        userdb=dev
+        password=Wc~0453224133
+        database=dev
+        #Backup Directory
+        backup_dir=dev_db
+#Remote/backup server information
+        user=backup
+        backup_server=218.50.181.107
+        remote_dir=/backup
+#--------------Dynamic Variables
 #get time and date
         prefix_date=$(date +%b_%d_%a"_"%H"."%M)
-#---------------------
-#Finding Prefix/labeling i.e Last_Wed_of_$backup_name
+#Find Prefix/labeling i.e Last_Wed_of_$backup_name
             week=$(date +%a)
             last_7day=$(date -d "`date +%Y%m01` +1 month -7days" +%d)
             today=$(date +%d)
             #if today > last 7 days of the Month
                 if [ $today -ge $last_7day ] && [ "$week" = "Wed" ];
                 then
-                #true
                 prefix_name="Last_Wednesday_of_"
                 else
                 prefix_name=""
                 fi
-#Put Parameters here
-#Database Parameters
-        userdb=
-        password=Wc~0453224133
-        database=dev
+        #Name of raw dumfile
         backup_name=$prefix_name$prefix_date$database"."sql
-#----------------------
-#Compress/Zip Variables
-#Name of backup when zipped
+        #Name of zipped backup file
         zip_name=$prefix_name$prefix_date$database"."tar.gz
 #---------------------
-#Remote server information
-#user, backup_server=domain name or ip address
-        user=backup
-        backup_server=218.50.181.107
-        remote_dir=/backup
 #Backing up process start
-    #this is your backup directory
-    backup_dir=dev_db
     #Scan if the directory exists then create it
     if [ -d "$backup_dir" ]; then
     # Will enter here if $DIRECTORY exists, even if it contains spaces
@@ -44,28 +41,23 @@
         else
         cd $backup_dir
     fi
-    if [ "$userdb" != "" ]; then
-    #create a database dump file
+    #create the database dump file
     mysqldump -u$userdb -p$password --single-transaction --quick --lock-tables=false $database > $backup_name
-    else
-    mysqldump -p$password --single-transaction --quick --lock-tables=false $database > $backup_name
-    fi
     #if successful
     if [ "$?" -eq 0 ]
     then
     #compress using gzip tar
         tar -cpzf $zip_name $backup_name
-        #Delete raw dump file if applicable
+        #Delete raw dump file
         if [ "$?" -eq 0 ]
-                then
+        then
                 #after zipping delete the raw dump file
                 rm -f *.sql
         fi
         #Send Backup Weekly to Remote Server
         if [ "$?" -eq 0 ] && [ $week = "Wed" ]
-                then
+        then
                 #Send the Backup
                 scp -q $zip_name $user@$backup_server:$remote_dir
-
         fi
     fi
