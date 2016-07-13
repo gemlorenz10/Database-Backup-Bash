e520cbc7068d3cc582314f6be7a032b72dfc8519
Gem Lorenz <gemlorenz10@gmail.com>
2016-07-14 02:31:55 +0800

    Update backupdevDB.sh

--- backupdevDB.sh
+++ backupdevDB.sh
@@ -1,7 +1,6 @@
 #!bin/bash
 #run this script on ~ directory of the current user
 #filename BackupDB.sh
-#filename sample for backup Prefix_JAN_10_Wed_1630DBbackup.tar.gz
 #-------------Static Variables
 #Database Variables
         userdb=dev
@@ -15,48 +14,42 @@
         remote_dir=/backup
 #--------------Dynamic Variables
 #get time and date
-        prefix_date=$(date +%b_%d_%a"_"%H"."%M)
         #Get the Last Wednesday Add it to as prefix
-        week=$(date +%a)
-        last_7day=$(date -d "`date +%Y%m01` +1 month -7days" +%d)
-        today=$(date +%d)
         #if today >= last 7 days of the Month
-                if [ $today -ge $last_7day ] && [ "$week" = "Wed" ];
-                then
-                prefix_name="Last_Wednesday_of_"
+        week=$(date +%a)
+                if [ "$week" = "Wed" ]; then
+                month=$(date +%Y_%m)
+                today="Last_Wednesday_of_"$month
                 else
-                prefix_name=""
+                today=$week
                 fi
-        #Name of raw dumfile
-        backup_name=$prefix_name$prefix_date$database"."sql
-        #Name of zipped backup file
-        zip_name=$prefix_name$prefix_date$database"."tar.gz
+        #backup destination
+        backup_dst=$backup_dir/$today
 #---------------------
 #Backing up process start
     #Scan if the directory exists then create it
-    if [ -d "$backup_dir" ]; then
-        cd $backup_dir
+    if [ -d "$backup_dst" ]; then
+        rm -f $backup_dst/*
         else
-        mkdir $backup_dir
-        cd $backup_dir
+        mkdir $backup_dst
     fi
     #create the database dump file
-    mysqldump -u$userdb -p$password --single-transaction --quick --lock-tables=false $database > $backup_name
+    mysqldump -u$userdb -p$password --single-transaction --quick --lock-tables=false $database > $backup_dst/$database.sql
     #if successful
     if [ "$?" -eq 0 ]
     then
     #compress using gzip tar
-        tar -cpzf $zip_name $backup_name
+        tar -cpzf $database.tar.gz $backup_dst/$database
         #Delete raw dump file
         if [ "$?" -eq 0 ]
         then
         #after zipping delete the raw dump file
-                rm -f *.sql
+                rm -f $backup_dst/*.sql
         fi
         #Send Backup Weekly to Remote Server
-        if [ "$?" -eq 0 ] && [ $week = "Wed" ]
+        if [ "$?" -eq 0 ]
         then
         #Send the Backup
-                scp -q $zip_name $user@$backup_server:$remote_dir
+                scp -q $database.tar.gz $user@$backup_server:$remote_dir
         fi
     fi
