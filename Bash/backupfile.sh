01dd8ffd26a381cbbb4fc0494406766dc5e8c173
Gem Lorenz <gemlorenz10@gmail.com>
2016-07-13 16:41:18 +0800

    adding date as prefix name

--- Bash/backupfile.sh
+++ Bash/backupfile.sh
@@ -2,15 +2,26 @@
 #run everytime you want to backup
 #dont forget to change the variables
 
-raw=file-upload
-backupfile=file-upload.tar.gz
+#$raw the raw file or directory you want to backup
+#$backupname wil be the name of the archived and zipped raw file
+#$remotedir is the directory in your remote backup server where you want your backup files to be saved
+#$date will be used as prefix name for backup files
+date=$(date +%a_%b_%d_)
+raw=/www/file-upload
+backupname=$date''file-upload.tar.gz
 remotedir=/backup/
-#test if rawfile exist
-#then
-tar -czpf $backupfile $rawfile 
-#test if zip is successful
-#then
-scp -q $backupfile backup@218.50.181.107:$remotedir
-#test if sending is successful
-#then
-rm $backupfile
+
+#compressed zip the raw file or directory
+tar -czpf $backupname $raw 
+if [ "$?" -eq 0 ];then
+	#Send it to backup server
+	scp -q $backupname backup@218.50.181.107:$remotedir
+		if [ "$?" -eq 0 ];then
+			#remove backup zipped on local machine 
+			rm $backupname
+		else
+			exit
+		fi
+else
+	exit	
+fi
