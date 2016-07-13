7d4c90d6a7167622be38b1483ee8af7c01210731
Gem Lorenz <gemlorenz10@gmail.com>
2016-07-13 20:53:48 +0800

    Update backupdevfile.sh

--- backupdevfile.sh
+++ backupdevfile.sh
@@ -1,25 +1,26 @@
 #!bin/bash
 #run everytime you want to backup
 #dont forget to change the variables
-#Backup everytime we finished an update or add feature on the website
 
-#$raw is the variable of the raw file or directory
+#$raw the raw file or directory you want to backup
 #$backupname wil be the name of the archived and zipped raw file
 #$remotedir is the directory in your remote backup server where you want your backup files to be saved
-raw=/www/wordpress
-backupname=wordpress.tar.gz
+#$date will be used as prefix name for backup files
+date=$(date +%a_%b_%d_)
+raw=/www/file-upload
+backupname=$date''file-upload.tar.gz
 remotedir=/backup/
+
 #compressed zip the raw file or directory
-tar -czpf $backupfile $rawfile 
+tar -czpf $backupname $raw 
 if [ "$?" -eq 0 ];then
-	#Send it to backup directory
+	#Send it to backup server
 	scp -q $backupname backup@218.50.181.107:$remotedir
 		if [ "$?" -eq 0 ];then
-			#remove backup zipped 
+			#remove backup zipped on local machine 
 			rm $backupname
 		else
 			exit
-			#if possible 
 		fi
 else
 	exit	
