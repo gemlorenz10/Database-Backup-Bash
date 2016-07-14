0652a7ed457c578d86dbde2e1081c9690713ae19
Gem Lorenz <gemlorenz10@gmail.com>
2016-07-14 22:29:41 +0800

    Update backupdevDB.sh

--- backupdevDB.sh
+++ backupdevDB.sh
@@ -5,53 +5,32 @@
         userdb=root
         password=root
         database=names_db
-#Remote/backup server information
-        user=root
-        backup_server=192.168.0.251
-        remote_dir=/backup
-        remote_bash=scandir.sh
-#--------------Dynamic Variables
+        backup_dst=backupdb
+        #--------------Dynamic Variables
 #get time and date
         #Get the Last Wednesday Add it to as prefix
         #if today >= last 7 days of the Month
         week=$(date +%A)
-                if [ "$week" = "Wed" ]; then
+                if [ "$week" = "Thursday" ]; then
                 month=$(date +%B_%Y)
                 #today directory represented as Monday, Thuesday, Last_Wednesday_of_January_2016 ..etc
-                today="Last_Wednesday_of_"$month
+                today="Last_Thrusday_of_"$month
                 else
                 today=$week
                 fi
-        #backup destination
-        #~/Monday
-        backup_dst=/$today
 #------------------------------------------------------
 #Backing up process start
     #Scan if the directory exists then create it
     if [ -d "$backup_dst" ]; then
-        rm -f $backup_dst/*
-        elseqq
+        n=1
+        else
         mkdir $backup_dst
     fi
     #create the database dump file
-    mysqldump -u$userdb -p$password --single-transaction --quick --lock-tables=false $database > $backup_dst/$database.sql
+    mysqldump -u$userdb -p$password --single-transaction --quick --lock-tables=false $database > $backup_dst/$today"_"$database.sql
     if [ "$?" -eq 0 ]
     then
     #compress using gzip tar
-        tar -cpzf $backup_dst/$database.sql.tar.gz $backup_dst/$database.sql
-        #echo tar
-        if [ "$?" -eq 0 ]
-        then
-        #after zipping delete the raw dump file
-                rm -f $backup_dst/*.sql
-                #echo rm
-        fi
+       gzip -qf $backup_dst/$today"_"$database.sql
         #Send Backup Everyday
-        #Make sure remote directories exists!
-        if [ "$?" -eq 0 ]
-        then
-                ssh 
-                #echo send
-                scp -q $backup_dst/$database.sql.tar.gz $user@$backup_server:$remote_dir/$backup_dst
-        fi
     fi
